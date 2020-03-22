b00b214b235520b29406d94d60ae3c925354f64c
antmdev <ant@antmilner.com>
2020-03-22 18:36:47 +0000

    lecture 47 completed

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -13,38 +13,38 @@
             <?php
 // //************************************************************************* 
 // // HOW TO CONNECT TO DB AND RETURN SOMETHING
-
 //                 $sql = "SELECT * FROM users WHERE id=1"; //make a query to DB and saved in variable $sql
 //                 $result = $database->query($sql); //use method from our class and pass in the string from the variable above
 //                 $user_found = mysqli_fetch_array($result); //pull out the results form the query using fetch_query and save the result to $user_found
 //                 echo $user_found['username']; //use the reult as an array because this is what is returned
-
 // //************************************************************************* 
-
-
 // $user = new User(); //istantiate the class (from user.php) so we can use the method
 //CHanged to static method so not required
+                    // while ($row = mysqli_fetch_array($result_set)) {
+                    //     echo $row['username'] . "<br>";
+                    // }
+                    //here we take the object from User class and assign a values
+                    //of the arrays to the properties
+                    // //but this would take a lot of time to do this each time, where as we could create a method that assigns the array properties to the object
+                    // $user->id = $found_user['id'];
+                    // $user->username = $found_user['username'];
+                    // $user->password = $found_user['password'];
+                    // $user->first_name = $found_user['first_name'];
+                    // $user->last_name = $found_user['last_name'];
+                    // echo $user->username;
+                    // $found_user = User::find_user_by_id(2); //1) bring in values from db
+                    // $user = User::instantiation($found_user); //2) istantiate the class so  we can use the properties
+                    // echo $user->username;
 
+                    $users = User::find_all_users();
 
-                    $result_set = User::find_all_users();
-                    while ($row = mysqli_fetch_array($result_set)) {
-                        echo $row['username'] . "<br>";
+                    foreach($users as $user){
+
+                        echo $user->id . "<br>";
                     }
 
-                    $found_user = User::find_user_by_id(2);
 
-                    $user = new User();
-
-                    $user->id = $found_user['id'];
-                    $user->username = $found_user['username'];
-                    $user->password = $found_user['password'];
-                    $user->first_name = $found_user['first_name'];
-                    $user->last_name = $found_user['last_name'];
-                
-                    echo $user->id;
-                    
-
-                ?>
+                ?> 
 
                         <ol class="breadcrumb">
                             <li>
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -3,6 +3,7 @@
 class User {
 
 
+    //create objects that we can control later (Admin content)
     public $id;
     public $username;
     public $password;
@@ -12,11 +13,10 @@ class User {
     public static function find_all_users() {
 
     return self::find_this_query("SELECT * FROM users");
+
     // return self::find_this_query("SELECT * FROM users");
     // global $database; //making the inherited class $database public 
     // $result_set = $database->query("SELECT * FROM users");
-    return $result_set;
-
     }
 
     public static function find_user_by_id($user_id) {
@@ -29,7 +29,46 @@ class User {
     public static function find_this_query($sql){
         global $database;
         $result_set = $database->query($sql);
-        return $result_set;
+        $the_object_array = array();                                //create an empty array to put objects
+
+        while($row = mysqli_fetch_array($result_set)){              //loop throughs the objects and the properties
+            $the_object_array[] = self::instantiation($row);        //bringing back objects and properties into the array
+        }
+        return $the_object_array;
+
+    }
+
+    //Automatically instantiate a class
+    //The_object will now instantiate itself
+    //this is the long version
+    public static function instantiation($the_record){ //get record from DB
+        
+        $the_object = new self; //means to instantiate a new version of the same class
+
+        // $the_object->id         = $found_user['id'];
+        // $the_object->username   = $found_user['username'];
+        // $the_object->password   = $found_user['password'];
+        // $the_object->first_name = $found_user['first_name'];
+        // $the_object->last_name  = $found_user['last_name'];
+
+        foreach ($the_record as $the_attribute => $value) {             //loop through and get key and value
+                                                                     //first check that the object has a property!
+            if($the_object->has_the_attribute($the_attribute)){     //if the key attribute exists
+                $the_object->$the_attribute = $value;               //then assign the object the value
+            }
+            
+            //we want to set the actual value of the $the_attribute variable. If you omit the $ sign you will literally set the the_attribute property on $the_object and we do not want to do that. We want the value of the $the_attribute to be set on the object, so that's why you need to put the $ sign in front of it. Remember that we are here setting the properties and it's values. 
+        }
+
+        return $the_object;
+
+    }
+
+    private function has_the_attribute($the_attribute){
+
+        $object_properties = get_object_vars($this); //need to pass in the existing class and use PHP function to grab all variables
+        return array_key_exists($the_attribute, $object_properties);
+        
 
     }
 }
